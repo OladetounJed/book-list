4b7084b2a8ec166e96b44378339900b31cf3d62a
OladetounJed <oladetountemitayo@gmail.com>
2020-06-22 15:44:21 +0100

    Task Completed (Without Locale Storage)

--- app.js
+++ app.js
@@ -1,27 +1,70 @@
-
-
 //Book Constructor
 function Book(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-};
-
-
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
 
 //UI COnstructor
 
-function Ui() {};
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const bookList = document.getElementById("book-list");
+
+  const row = document.createElement("tr");
+  row.innerHTML = `
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td><a href="#" class="delete">X</a></td>`;
+
+  bookList.appendChild(row);
+};
+
+UI.prototype.clearFields = function() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+UI.prototype.showAlert = function(message, className) {
+  const div = document.createElement("div");
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+
+  const container = document.querySelector(".container");
+  const form = document.getElementById("book-form");
+
+  container.insertBefore(div, form);
+
+  setTimeout(function() {
+    document.querySelector(".alert").remove();
+  }, 1000);
+};
 
 //Event Listeners
-document.getElementsByClassName('book-form').addEventListener('submit', function(e){
-    const title = document.getElementById("title").value,
-          author = document.getElementById("author").value,
-          isbn = document.getElementById("isbn").value;
-          
-          console.log(title, author, isbn)
+bookForm = document.getElementById("book-form");
 
-          preventDefault();
-})
+bookForm.addEventListener("submit", function(e) {
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
 
+  //Instantiate Book
+  const book = new Book(title, author, isbn);
 
+  //Instantiate UI
+
+  const ui = new UI();
+
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("Please Fill in all Fields", "error");
+  } else {
+    ui.addBookToList(book);
+    ui.clearFields();
+    ui.showAlert("Record Added", "success");
+  }
+
+  e.preventDefault();
+});
